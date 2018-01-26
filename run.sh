d61f5a889ce5585783772855a39e24540438f4ef
Lukas Prettenthaler <lukas@noenv.com>
2018-01-26 15:00:08 +0100

    inc version

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Droot.level=INFO -jar ./target/nassh-relay-1.0.4-fat.jar -conf ./src/docker/config.json -cp .
+java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Droot.level=INFO -jar ./target/nassh-relay-1.0.5-fat.jar -conf ./src/docker/config.json -cp .
